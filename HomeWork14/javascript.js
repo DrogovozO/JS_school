09c1031988e65bf032e44a32485db0005197024b
DrogovozO <drogovoz.olga.92@gmail.com>
2016-01-27 18:44:38 +0200

    simple plugin with jQuery

--- HomeWork14/javascript.js
+++ HomeWork14/javascript.js
@@ -1,34 +1,25 @@
-(function IIFE() {
+(function IIFE ($) {
     'use strict';
-    var $divModal = $('<div id="modal-dialog">'),
-        $buttonClose = $('<button class="close"><span>×</span>'),
-        $headerText = $('<h2>Sign-up form</h2>'),
-        $formModal = $('<form class="modal-form">'),
-        $divField = $('<div class="field">'),
-        $divFieldName = $('<div class="field-group">'),
-        $divFieldEmail = $('<div class="field-group">'),
-        $labelFName = $('<label for="FName">First Name</label>'),
-        $inputFName = $('<input name="FName" class="input-form" type="text">'),
-        $labelEmail = $('<label for="Email">Email Address</label>'),
-        $inputEmail = $('<input name="Email" class="input-form" type="email">'),
-        $divCheck = $('<div class="checkIn">'),
-        $inputSub = $('<input type="submit">');
+    var $divOverlay = $('#overlay');
+    $('.close').on('click', hideModal);
+    function hideModal () {
+        $divOverlay.hide();
+    }
 
-    $('.btn-sign-up').on('click', function (){
-        $divModal.toggle();
-    });
+    function showModal () {
+        $divOverlay.show();
+    }
 
+    $.fn.myModal = function (action) {
+        if( action == 'hide') {
+            hideModal(this);
+        } else if (action == 'show') {
+            showModal(this);
+        } else {
+           this.on('click', showModal.bind(this, this));
+        }
+    };
 
-    $($divField).append($divFieldName, $divFieldEmail);
-    $($divFieldName).append($labelFName, $inputFName);
-    $($divFieldEmail).append($labelEmail, $inputEmail);
-    $($divCheck).append($inputSub);
-    $($formModal).append($divField, $divCheck);
-    $($divModal).append($buttonClose, $headerText, $formModal);
-    $('body').append($divModal);
-    $divModal.css('display', 'none');
+})(jQuery);
 
-    $buttonClose.on('click', function (){
-        $divModal.hide();
-    });
-})();
+$('.btn-sign-up').myModal();
